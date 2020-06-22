a4cec37b60fee8b01c1bb4d74cb685128c8276d5
cynthia <cxla2018@mymail.pomona.edu>
2020-06-22 15:31:52 -0700

    better restitution, maybe

--- prototypes/paddles/src/main.rs
+++ prototypes/paddles/src/main.rs
@@ -8,13 +8,15 @@ use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::WindowBuilder;
 use winit_input_helper::WinitInputHelper;
 use ultraviolet::{Vec2, Vec3, geometry::Aabb};
-const WIDTH: u8 = 255;
+
+const WIDTH: u8 = 245;
 const HEIGHT: u8 = 255;
 const PADDLE_OFF_X: u8 = 16;
 const PADDLE_HEIGHT: u8 = 48;
 const PADDLE_WIDTH: u8 = 8;
 const BALL_SIZE: u8 = 8;
 
+// TODO: see comment on merge request
 struct WinitControl {
     _actors: Vec<Player>,
     // loci: Vec<>,
@@ -26,11 +28,14 @@ impl WinitControl {
     fn new() -> Self {
         Self {
             _actors: vec![Player::P1, Player::P2],
+            // loci of control?
+            // mapping: buttons -> variables. this button sets this variable to true when it's
+            // pressed, held down, etc
             selected_actions: Vec::new(),
         }
     }
 
-// handles keyboard inputs
+    // handles keyboard inputs
     fn update(&mut self, input:&WinitInputHelper) {
         self.selected_actions.clear();
         if input.key_held(VirtualKeyCode::Q) {
@@ -57,6 +62,7 @@ struct PongPhysics {
     positions:Vec<Vec2>,
     velocities:Vec<Vec2>
 }
+
 impl PongPhysics {
     fn new() -> Self {
         Self {
@@ -72,19 +78,39 @@ impl PongPhysics {
     }
 }
 
-// velocity shifts bodies out of whatever they interpenetrated before
-// moving
-struct PongCollision {
+struct AabbCollision {
     bodies: Vec<Aabb>,
     velocities: Vec<Vec2>,
+    metadata: Vec<CollisionData<CollisionID>>,
     contacts: Vec<(usize, usize)>,
 }
 
-impl PongCollision {
+#[derive(Default, Clone, Copy)]
+struct CollisionData<ID: Copy + Eq> {
+    solid: bool, // true = participates in restitution, false = no
+    fixed: bool, // collision system cannot move it
+    id: ID
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum CollisionID {
+    Paddle(Player),
+    Ball,
+    TopWall,
+    BottomWall,
+    SideWall(Player),
+}
+
+impl Default for CollisionID {
+    fn default() -> Self { Self::Ball }
+}
+
+impl AabbCollision {
     fn new() -> Self {
         Self {
             bodies: Vec::new(),
             velocities: Vec::new(),
+            metadata: Vec::new(),
             contacts: Vec::new(),
         }
     }
@@ -92,29 +118,113 @@ impl PongCollision {
     fn update(&mut self) {
         self.contacts.clear();
         for (i, body) in self.bodies.iter().enumerate() {
-            for (j, body2) in self.bodies.iter().enumerate() {
-                if i != j && body.intersects(body2) {
-                    self.contacts.push((i, j));
+            for (j, body2) in self.bodies[i + 1..].iter().enumerate() {
+                if body.intersects(body2) {
+                    self.contacts.push((i, j + i + 1));
                 }
             }
         }
 
-        for (i, ..) in self.contacts.iter() {
-            let body = &mut self.bodies[*i];
-            let vel = &mut self.velocities[*i];
-            body.min.x -= vel.x;
-            body.min.y -= vel.y;
-            body.max.x -= vel.x;
-            body.max.y -= vel.y;
+        for (i, j) in self.contacts.iter() {
+            if !self.metadata[*i].solid && !self.metadata[*j].solid {
+                if !self.metadata[*i].fixed && !self.metadata[*j].fixed {
+                    let vel_i_x = self.velocities[*i].x / (self.velocities[*i].x + self.velocities[*j].x);
+                    let vel_i_y = self.velocities[*i].y / (self.velocities[*i].y + self.velocities[*j].y);
+                    let vel_j_x = self.velocities[*j].x / (self.velocities[*i].x + self.velocities[*j].x);
+                    let vel_j_y = self.velocities[*j].y / (self.velocities[*j].y + self.velocities[*i].y);
+                    if self.bodies[*i].max.x - self.bodies[*j].min.x < self.bodies[*j].max.x - self.bodies[*i].min.x {
+                        let displacement = self.bodies[*i].max.x - self.bodies[*j].min.x;
+                        self.bodies[*i].min.x += displacement * vel_i_x;
+                        self.bodies[*i].max.x += displacement * vel_i_x;
+                        self.bodies[*j].min.x += displacement * vel_j_x;
+                        self.bodies[*j].max.x += displacement * vel_j_x;
+                    } else {
+                        let displacement = self.bodies[*j].max.x - self.bodies[*i].min.y;
+                        self.bodies[*i].min.x -= displacement * vel_i_x;
+                        self.bodies[*i].max.x -= displacement * vel_i_x;
+                        self.bodies[*j].min.x -= displacement * vel_j_x;
+                        self.bodies[*j].max.x -= displacement * vel_j_x;
+                    }
+
+                    if self.bodies[*i].max.y - self.bodies[*j].min.y < self.bodies[*j].max.y - self.bodies[*i].min.y {
+                        let displacement = self.bodies[*i].max.y - self.bodies[*j].min.y;
+                        self.bodies[*i].min.y += displacement * vel_i_y;
+                        self.bodies[*i].max.y += displacement * vel_i_y;
+                        self.bodies[*j].min.y += displacement * vel_j_y;
+                        self.bodies[*j].max.y += displacement * vel_j_y;
+                    } else {
+                        let displacement = self.bodies[*j].max.y - self.bodies[*i].min.y;
+                        self.bodies[*i].min.y -= displacement * vel_i_y;
+                        self.bodies[*i].max.y -= displacement * vel_i_y;
+                        self.bodies[*j].min.y -= displacement * vel_j_y;
+                        self.bodies[*j].max.y -= displacement * vel_j_y;
+                    }
+                } else if !self.metadata[*i].fixed {
+                    if self.bodies[*i].max.x - self.bodies[*j].min.x < self.bodies[*j].max.x - self.bodies[*i].min.x {
+                        let displacement = self.bodies[*i].max.x - self.bodies[*j].min.x;
+                        self.bodies[*i].min.x -= displacement;
+                        self.bodies[*i].max.x -= displacement;
+                    } else {
+                        let displacement = self.bodies[*j].max.x - self.bodies[*i].min.x;
+                        self.bodies[*i].min.x += displacement;
+                        self.bodies[*i].max.x += displacement;
+                    }
+
+                    if self.bodies[*i].max.y - self.bodies[*j].min.y < self.bodies[*j].max.y - self.bodies[*i].min.y {
+                        let displacement = self.bodies[*i].max.y - self.bodies[*j].min.y;
+                        self.bodies[*i].min.y -= displacement;
+                        self.bodies[*i].max.y -= displacement;
+                    } else {
+                        let displacement = self.bodies[*j].max.y - self.bodies[*i].min.y;
+                        self.bodies[*i].min.y += displacement;
+                        self.bodies[*i].max.y += displacement;
+                    }
+                } else if !self.metadata[*j].fixed {
+                    if self.bodies[*i].max.x - self.bodies[*j].min.x < self.bodies[*j].max.x - self.bodies[*i].min.x {
+                        let displacement = self.bodies[*i].max.x - self.bodies[*j].min.x;
+                        self.bodies[*j].min.x += displacement;
+                        self.bodies[*j].max.x += displacement;
+                    } else {
+                        let displacement = self.bodies[*j].max.x - self.bodies[*i].min.x;
+                        self.bodies[*j].min.x -= displacement;
+                        self.bodies[*j].max.x -= displacement;
+                    }
+
+                    if self.bodies[*i].max.y - self.bodies[*j].min.y < self.bodies[*j].max.y - self.bodies[*i].min.y {
+                        let displacement = self.bodies[*i].max.y - self.bodies[*j].min.y;
+                        self.bodies[*j].min.y += displacement;
+                        self.bodies[*j].max.y += displacement;
+                    } else {
+                        let displacement = self.bodies[*j].max.y - self.bodies[*i].min.y;
+                        self.bodies[*j].min.y -= displacement;
+                        self.bodies[*j].max.y -= displacement;
+                    }
+
+                }
+            }
         }
     }
 }
+
+/* dont worry abt this for now
+struct PongResources {
+}
+
+impl PongResources {
+    fn new() -> Self {
+    }
+
+    fn change(amt: i8) {
+    }
+}
+*/
 
 
 struct Logics {
     control: WinitControl,
     physics: PongPhysics,
-    collision: PongCollision,
+    collision: AabbCollision,
+//    resources: PongResources,
 }
 
 impl Logics {
@@ -122,25 +232,31 @@ impl Logics {
         Self {
             control: WinitControl::new(),
             physics: PongPhysics::new(),
-            collision: PongCollision::new(),
+            collision: AabbCollision::new(),
+//            resources: PongResources::new(),
         }
     }
 }
 
+#[derive(Clone, Copy, PartialEq, Eq)]
 enum Player {
     P1,
     P2
 }
+
 enum Action {
     Move(i8),
     Serve
 }
+
+
 struct World {
     paddles: (u8, u8),
     ball: (u8, u8),
     ball_err: Vec2,
     ball_vel: Vec2,
-    serving: Option<Player>
+    serving: Option<Player>,
+    points: (u8, u8),
 }
 
 
@@ -172,8 +288,8 @@ fn main() -> Result<(), Error> {
             world.draw(pixels.get_frame());
             if pixels
                 .render()
-                .map_err(|e| panic!("pixels.render() failed: {}", e))
-                .is_err()
+                    .map_err(|e| panic!("pixels.render() failed: {}", e))
+                    .is_err()
             {
                 *control_flow = ControlFlow::Exit;
                 return;
@@ -212,7 +328,8 @@ impl World {
             ball: (WIDTH/2-BALL_SIZE/2, HEIGHT/2-BALL_SIZE/2),
             ball_err: Vec2::new(0.0,0.0),
             ball_vel: Vec2::new(0.0,0.0),
-            serving: Some(Player::P1)
+            serving: Some(Player::P1),
+            points: (0, 0),
         }
     }
 
@@ -220,29 +337,19 @@ impl World {
         logics.control.update(input);
         for choice in logics.control.selected_actions.iter() {
             match choice {
-                (Player::P1, Action::Move(amt)) => {
-                    if *amt < 0 {
-                        self.paddles.0 -= amt.abs() as u8;
-                    } else {
-                        self.paddles.0 += *amt as u8;
-                    }
-                },
+                (Player::P1, Action::Move(amt)) => 
+                    self.paddles.0 = ((self.paddles.0 as i16 + *amt as i16).max(0) as u8).min(255 - PADDLE_HEIGHT),
                 (Player::P1, Action::Serve) => {
                     if let Some(Player::P1) = self.serving {
-                        self.ball_vel = Vec2::new(3.0, 3.0);
+                        self.ball_vel = Vec2::new(2.0, 2.0);
                         self.serving = None;
                     }
                 },
-                (Player::P2, Action::Move(amt)) => {
-                    if *amt < 0 {
-                        self.paddles.1 -= amt.abs() as u8;
-                    } else {
-                        self.paddles.1 += *amt as u8;
-                    }
-                },
+                (Player::P2, Action::Move(amt)) => 
+                    self.paddles.1 = ((self.paddles.1 as i16 + *amt as i16).max(0) as u8).min(255 - PADDLE_HEIGHT),
                 (Player::P2, Action::Serve) => {
                     if let Some(Player::P2) = self.serving {
-                        self.ball_vel = Vec2::new(-3.0, -3.0);
+                        self.ball_vel = Vec2::new(-2.0, -2.0);
                         self.serving = None;
                     }
                 }
@@ -269,31 +376,25 @@ impl World {
         // game specific collision stuff
         for contact in logics.collision.contacts.iter() {
             match (contact.0, contact.1) {
-                // collisions: wall x wall = nothing.
-                // paddle x wall = stop (nothing)
-                // ball x top, bottom wall = bounce (x-axis)
-                // ball x paddle = bounce (y-axis)
-                // operate on contact.0, compare w/ contact.1
-                (4, 0) | (4, 1) => {
+                (0, 4) | (1, 4) => {
                     self.ball_vel = Vec2::new(0.0, 0.0);
                     self.ball = (WIDTH / 2 - BALL_SIZE / 2, HEIGHT / 2 - BALL_SIZE / 2);
-                    if contact.1 == 0 {
+                    if contact.0 == 0 {
                         self.serving = Some(Player::P2);
-                        println!("p2 scores");
+                        self.points.1 += 1;
+                        println!("p2 scores. p1: {}, p2: {}",
+                            self.points.0,
+                            self.points.1);
                     } else {
                         self.serving = Some(Player::P1);
-                        println!("p1 scores");
+                        self.points.0 += 1;
+                        println!("p1 scores. p1: {} p2: {}",
+                            self.points.0,
+                            self.points.1);
                     }
                 },
-                (4, 2) | (4, 3) => {
-                    self.ball_vel.y *= -1.0;
-                    self.ball.1 = (self.ball.1 as f32 + self.ball_vel.y.trunc()) as u8;
-                },
-                (4, 5) | (4, 6) => {
-                    self.ball_vel.x *= -1.0;
-                    self.ball.0 = (self.ball.0 as f32 + self.ball_vel.x.trunc()) as u8;
-
-                },
+                (2, 4) | (3, 4) => self.ball_vel.y *= -1.0,
+                (4, 5) | (4, 6) => self.ball_vel.x *= -1.0,
                 _ => {}
             }
         }
@@ -313,36 +414,33 @@ impl World {
         self.ball_vel = physics.velocities[0];
     }
 
-    fn project_collision(&self, collision: &mut PongCollision, control: &WinitControl) {
+    fn project_collision(&self, collision: &mut AabbCollision, control: &WinitControl) {
         collision.bodies.resize_with(7, Aabb::default);
         collision.velocities.resize_with(7, Default::default);
+        collision.metadata.resize_with(7, CollisionData::default);
         let colliders = [
             Aabb::new(
                 Vec3::new(-1.0, 0.0, 0.0),
                 Vec3::new(0.0, HEIGHT as f32, 0.0)),
-            // right
             Aabb::new(
                 Vec3::new(WIDTH as f32, 0.0, 0.0),
                 Vec3::new(WIDTH as f32 + 1.0, HEIGHT as f32, 0.0)),
-            // top
             Aabb::new(
                 Vec3::new(0.0, -1.0, 0.0),
                 Vec3::new(WIDTH as f32, 0.0, 0.0)),
-            // bottom
             Aabb::new(
                 Vec3::new(0.0, HEIGHT as f32, 0.0),
                 Vec3::new(WIDTH as f32, HEIGHT as f32 + 1.0, 0.0)),
-            // ball
             Aabb::new(
                 Vec3::new(self.ball.0 as f32, self.ball.1 as f32, 0.0),
                 Vec3::new(self.ball.0 as f32 + BALL_SIZE as f32, self.ball.1 as f32 + BALL_SIZE as f32, 0.0)),
-            // paddles
             Aabb::new(
                 Vec3::new((PADDLE_OFF_X + PADDLE_WIDTH - 1) as f32, self.paddles.0 as f32, 0.0),
                 Vec3::new((PADDLE_OFF_X + PADDLE_WIDTH) as f32, self.paddles.0 as f32 + PADDLE_HEIGHT as f32, 0.0)),
             Aabb::new(
                 Vec3::new((WIDTH - PADDLE_OFF_X - PADDLE_WIDTH) as f32, self.paddles.1 as f32, 0.0),
                 Vec3::new((WIDTH - PADDLE_OFF_X - PADDLE_WIDTH + 1) as f32, self.paddles.1 as f32 + PADDLE_HEIGHT as f32, 0.0))];
+
         for (i, body) in colliders.iter().enumerate() {
             collision.bodies[i] = *body;
         }
@@ -365,13 +463,23 @@ impl World {
         }
         collision.velocities[5] = Vec2::new(0.0, p1_vel);
         collision.velocities[6] = Vec2::new(0.0, p2_vel);
+
+        let metadata = [
+            CollisionData{ solid: true, fixed: true, id: CollisionID::SideWall(Player::P1) },
+            CollisionData{ solid: true, fixed: true, id: CollisionID::SideWall(Player::P2) },
+            CollisionData{ solid: true, fixed: true, id: CollisionID::TopWall },
+            CollisionData{ solid: true, fixed: true, id: CollisionID::BottomWall },
+            CollisionData{ solid: true, fixed: false, id: CollisionID::Ball },
+            CollisionData{ solid: true, fixed: true, id: CollisionID::Paddle(Player::P1) },
+            CollisionData{ solid: true, fixed: true, id: CollisionID::Paddle(Player::P2) }];
+        for (i, data) in metadata.iter().enumerate() {
+            collision.metadata[i] = *data;
+        }
     }
 
-    fn unproject_collision(&mut self, collision: &PongCollision) {
+    fn unproject_collision(&mut self, collision: &AabbCollision) {
         self.ball.0 = collision.bodies[4].min.x.trunc() as u8;
         self.ball.1 = collision.bodies[4].min.y.trunc() as u8;
-        self.paddles.0 = collision.bodies[5].min.y.trunc() as u8;
-        self.paddles.1 = collision.bodies[6].min.y.trunc() as u8;
     }
 
     /// Draw the `World` state to the frame buffer.
@@ -382,17 +490,17 @@ impl World {
             pixel.copy_from_slice(&[0,0,128,255]);
         }
         draw_rect(PADDLE_OFF_X, self.paddles.0,
-                  PADDLE_WIDTH, PADDLE_HEIGHT,
-                  [255,255,255,255],
-                  frame);
+            PADDLE_WIDTH, PADDLE_HEIGHT,
+            [255,255,255,255],
+            frame);
         draw_rect(WIDTH-PADDLE_OFF_X-PADDLE_WIDTH, self.paddles.1,
-                  PADDLE_WIDTH, PADDLE_HEIGHT,
-                  [255,255,255,255],
-                  frame);
+            PADDLE_WIDTH, PADDLE_HEIGHT,
+            [255,255,255,255],
+            frame);
         draw_rect(self.ball.0, self.ball.1,
-                  BALL_SIZE, BALL_SIZE,
-                  [255,255,255,255],
-                  frame);
+            BALL_SIZE, BALL_SIZE,
+            [255,255,255,255],
+            frame);
     }
 }
 fn draw_rect(x:u8, y:u8, w:u8, h:u8, color:[u8;4], frame:&mut [u8]) {
