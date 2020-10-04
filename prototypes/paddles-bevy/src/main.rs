ae47c8de93edc7dfb60ed71727f96b0556a59b18
cynthia <cxla2018@mymail.pomona.edu>
2020-10-04 00:08:45 -0700

    write physics and control logics in bevy
    ok [sighs deeply] i kind of see how bevy works now but the staging stuff
    (lines 85-95) makes me want to cry since we _really_ don't want to run
    everything in parallel, but making one update function would make the
    calls to all the resources/queries really messy, i think.
    
    note that the bounds stuff isn't quite working properly yet because a)
    the screen is way bigger than the paddles screen and i haven't figured
    out how to change it, and b) i haven't done collision yet

--- prototypes/paddles-bevy/src/main.rs
+++ prototypes/paddles-bevy/src/main.rs
@@ -54,12 +54,11 @@ enum Player {
 }
 
 struct Ball {
-    x: f32,
-    y: f32,
+    vel: Vec2
 }
 
 struct Paddle {
-    y: f32,
+    player: Player
 }
 
 struct World {
@@ -70,22 +69,36 @@ struct World {
     score: (u8, u8)
 }
 
+struct Serving {
+    serving: Option<Player>
+}
+
 
 fn main() {
-    let world = World::new();
+    // let world = World::new();
     let logics = Logics::new();
     App::build()
         .add_default_plugins()
-        .add_resource(world)
         .add_resource(logics)
+        .add_resource(Serving { serving: Some(Player::P1) })
         .add_startup_system(setup.system())
+        .add_system_to_stage(stage::UPDATE, project_control.system())
+        .add_stage_after(stage::UPDATE, "up_control")
+        .add_system_to_stage("up_control", update_control.system())
+        .add_stage_after("up_control", "unproj_control")
+        .add_system_to_stage("unproj_control", unproject_control.system())
+        .add_stage_after("unproj_control", "proj_physics")
+        .add_system_to_stage("proj_physics", project_physics.system())
+        .add_stage_after("proj_physics", "up_physics")
+        .add_system_to_stage("up_physics", update_physics.system())
+        .add_stage_after("up_physics", "unproj_physics")
+        .add_system_to_stage("unproj_physics", unproject_physics.system())
         .run();
 }
 
 fn setup(
     mut commands: Commands,
     mut materials: ResMut<Assets<ColorMaterial>>,
-    world: Res<World>,
 ) {
     // setup logics
 
@@ -97,77 +110,186 @@ fn setup(
         .spawn(SpriteComponents {
             material: materials.add(Color::rgb(1.0, 1.0, 1.0).into()),
             transform: Transform::from_translation(
-                Vec3::new(
-                    -(PADDLE_OFF_X as f32),
-                    world.paddles.0.y,
-                    0.0
-                )
-            ),
+                Vec3::new(-(PADDLE_OFF_X as f32), 0.0, 0.0)),
             sprite: Sprite::new(Vec2::new(PADDLE_WIDTH as f32, PADDLE_HEIGHT as f32)),
             ..Default::default()
         })
-        .with(Paddle {y: 0.0})
+        .with(Paddle { player: Player::P1 })
         // paddle 2
         .spawn(SpriteComponents {
             material: materials.add(Color::rgb(1.0, 1.0, 1.0).into()),
             transform: Transform::from_translation(
-                Vec3::new(
-                    PADDLE_OFF_X as f32,
-                    world.paddles.1.y,
-                    0.0
-                )
-            ),
+                Vec3::new(PADDLE_OFF_X as f32, 0.0, 0.0)),
             sprite: Sprite::new(Vec2::new(PADDLE_WIDTH as f32, PADDLE_HEIGHT as f32)),
             ..Default::default()
         })
-        .with(Paddle {y: 0.0})
+        .with(Paddle { player: Player::P2 })
+        // ball
         .spawn(SpriteComponents {
             material: materials.add(Color::rgb(1.0, 0.75, 0.0).into()),
             transform: Transform::from_translation(
-                Vec3::new(
-                    world.ball.x,
-                    world.ball.y,
-                    0.0
-                )
-            ),
+                Vec3::new(0.0, 0.0, 0.0)),
             sprite: Sprite::new(Vec2::new(BALL_SIZE as f32, BALL_SIZE as f32)),
             ..Default::default()
         })
-        .with(Ball {x: 0.0, y: 0.0});
+        .with(Ball { vel: Vec2::new(0.0, 0.0) });
 }
 
-
-fn update(
-    mut world: ResMut<World>,
+fn project_control(
+    serving: Res<Serving>,
     mut logics: ResMut<Logics>,
-    input: Res<Input<KeyCode>>) {
-    world.update(&mut logics, input);
+) {
+    let control = &mut logics.control;
+    control.mapping[0][0].is_valid = true;
+    control.mapping[0][1].is_valid = true;
+    control.mapping[1][0].is_valid = true;
+    control.mapping[1][1].is_valid = true;
+
+    if let Some(player) = serving.serving {
+        match player {
+            Player::P1 => control.mapping[0][2].is_valid = true,
+            Player::P2 => control.mapping[1][2].is_valid = true,
+        }
+    } else {
+        control.mapping[0][2].is_valid = false;
+        control.mapping[1][2].is_valid = false;
+    }
+}
+
+fn update_control(mut logics: ResMut<Logics>, input: Res<Input<KeyCode>>) {
+    logics.control.update(&input);
 }
 
 
-impl World {
-    fn new() -> Self {
-        Self {
-            paddles: (Paddle {y: 0.0}, Paddle {y: 0.0}),
-            ball: Ball {x: 0.0, y: 0.0},
-            ball_vel: Vec2::new(0.0, 0.0),
-            serving: Some(Player::P1),
-            score: (0, 0),
+fn unproject_control(
+    logics: Res<Logics>,
+    mut serving: ResMut<Serving>,
+    mut ball_query: Query<(&mut Ball, &Transform)>,
+    mut paddles_query: Query<(&Paddle, &mut Transform)>
+) {
+    let control = &logics.control;
+    for (paddle, mut transform) in &mut paddles_query.iter() {
+        let translation = transform.translation_mut();
+        match paddle.player {
+            Player::P1 => {
+                *translation.y_mut() += control.values[0][0].value
+                    - control.values[0][1].value;
+                *translation.y_mut() = translation.y()
+                .max(0.0).min(255.0 - PADDLE_HEIGHT as f32);
+            }
+            Player::P2 => {
+                *translation.y_mut() += control.values[1][0].value
+                    - control.values[1][1].value;
+                *translation.y_mut() = translation.y()
+                .max(0.0).min(255.0 - PADDLE_HEIGHT as f32);
+            }
         }
     }
 
-    fn update(&mut self,
-        logics: &mut Logics,
-        input: Res<Input<KeyCode>>) {
-        self.project_control(&mut logics.control);
-        logics.control.update(&input);
-        self.unproject_control(&logics.control);
+    for (mut ball, _) in &mut ball_query.iter() {
+        if let Some(player) = serving.serving {
+            match player {
+                Player::P1 => {
+                    let values = &control.values[0][2];
+                    if values.changed_by == 1.0 && values.value != 0.0 {
+                        ball.vel = Vec2::new(1.0, 1.0);
+                        serving.serving = None;
+                    }
+                }
+                Player::P2 => {
+                    let values = &control.values[1][2];
+                    if values.changed_by == 1.0 && values.value != 0.0 {
+                        ball.vel = Vec2::new(-1.0, -1.0);
+                        serving.serving = None;
+                    }
+                }
+            }
+        }
+    }
+}
 
-        self.project_physics(&mut logics.physics);
-        logics.physics.update();
-        self.unproject_physics(&logics.physics);
+fn project_physics(
+    mut logics: ResMut<Logics>,
+    (ball, transform): (&Ball, &Transform),
+) {
+    let physics = &mut logics.physics;
+    physics.positions.resize_with(1, Vec2::default);
+    physics.velocities.resize_with(1, Vec2::default);
+    physics.accelerations.resize_with(1, Vec2::default);
+    physics.add_physics_entity(0,
+        {
+            let translation = transform.translation();
+            Vec2::new(translation.x(), translation.y())
+        },
+        ball.vel,
+        Vec2::new(0.0, 0.0));
+}
 
-        /* self.project_collision(&mut logics.collision, &logics.control);
+fn update_physics(mut logics: ResMut<Logics>) {
+    logics.physics.update();
+}
+
+fn unproject_physics(
+    logics: Res<Logics>,
+    mut ball_query: Query<(&mut Ball, &mut Transform)>,
+) {
+    let physics = &logics.physics;
+    for (mut ball, mut transform) in &mut ball_query.iter() {
+        ball.vel = physics.velocities[0];
+        transform.translate(Vec3::new(
+                ball.vel.x(),
+                ball.vel.y(),
+                0.0));
+    }
+}
+
+
+impl Logics {
+    fn new() -> Self {
+        Self {
+            control: {
+                let mut control = BevyKeyboardControl::new();
+                control.add_key_map(0,
+                    KeyCode::Q,
+                    ActionID::MoveUp(Player::P1),
+                );
+                control.add_key_map(0,
+                    KeyCode::A,
+                    ActionID::MoveDown(Player::P1),
+                );
+                control.add_key_map(0,
+                    KeyCode::W,
+                    ActionID::Serve(Player::P1),
+                );
+                control.add_key_map(1,
+                    KeyCode::O,
+                    ActionID::MoveUp(Player::P2),
+                );
+                control.add_key_map(1,
+                    KeyCode::L,
+                    ActionID::MoveDown(Player::P2),
+                );
+                control.add_key_map(1,
+                    KeyCode::I,
+                    ActionID::Serve(Player::P2),
+                );
+                control
+            },
+            physics: PointPhysics::new(),
+            collision: AabbCollision::new(),
+            resources: {
+                let mut resources = QueuedResources::new();
+                resources.items.insert( PoolID::Points(Player::P1), 0.0 );
+                resources.items.insert( PoolID::Points(Player::P2), 0.0 );
+                resources
+            },
+        }
+    }
+}
+
+/*
+ * this is the old world.update() function
+        self.project_collision(&mut logics.collision, &logics.control);
         logics.collision.update();
         self.unproject_collision(&logics.collision);
 
@@ -230,69 +352,11 @@ impl World {
                     }
                 }
             }
-        }*/
-    }
-
-    fn project_control(&self, control: &mut BevyKeyboardControl<ActionID>) {
-        control.mapping[0][0].is_valid = true;
-        control.mapping[0][1].is_valid = true;
-        control.mapping[1][0].is_valid = true;
-        control.mapping[1][1].is_valid = true;
-
-        if (self.ball_vel.x(), self.ball_vel.y()) == (0.0, 0.0) {
-            match self.serving {
-                Some(Player::P1) => control.mapping[0][2].is_valid = true,
-                Some(Player::P2) => control.mapping[1][2].is_valid = true,
-                None => {}
-            }
-        } else {
-            control.mapping[0][2].is_valid = false;
-            control.mapping[1][2].is_valid = false;
         }
-    }
 
-    fn unproject_control(&mut self, control: &BevyKeyboardControl<ActionID>) {
-        self.paddles.0.y =
-            (self.paddles.0.y - control.values[0][0].value + control.values[0][1].value)
-            .max(0.0).min(255.0 - PADDLE_HEIGHT as f32);
-        self.paddles.1.y = (self.paddles.1.y - control.values[1][0].value + control.values[1][1].value)
-            .max(0.0).min(255.0 - PADDLE_HEIGHT as f32);
-        if (self.ball_vel.x(), self.ball_vel.y()) == (0.0, 0.0) {
-            match self.serving {
-                Some(Player::P1) => {
-                    let values = &control.values[0][2];
-                    if values.changed_by == 1.0 && values.value != 0.0 {
-                        self.ball_vel = Vec2::new(1.0, 1.0);
-                    }
-                }
-                Some(Player::P2) => {
-                    let values = &control.values[1][2];
-                    if values.changed_by == 1.0 && values.value != 0.0 {
-                        self.ball_vel = Vec2::new(-1.0, -1.0);
-                    }
-                }
-                None => {}
-            }
-        }
-    }
+these are project/unproject fns
 
-    fn project_physics(&self, physics: &mut PointPhysics<Vec2>) {
-        physics.positions.resize_with(1, Vec2::default);
-        physics.velocities.resize_with(1, Vec2::default);
-        physics.accelerations.resize_with(1, Vec2::default);
-        physics.add_physics_entity(0,
-            Vec2::new(self.ball.x, self.ball.y),
-            self.ball_vel,
-            Vec2::new(0.0, 0.0));
-    }
-
-    fn unproject_physics(&mut self, physics: &PointPhysics<Vec2>) {
-        self.ball.x = physics.positions[0].x().max(0.0).min((WIDTH - BALL_SIZE) as f32);
-        self.ball.y = physics.positions[0].y().max(0.0).min((HEIGHT - BALL_SIZE) as f32);
-        self.ball_vel = physics.velocities[0];
-    }
-
-/*    fn project_collision(&self, collision: &mut AabbCollision<CollisionID>, control: &BevyKeyboardControl<ActionID>) {
+    fn project_collision(&self, collision: &mut AabbCollision<CollisionID>, control: &BevyKeyboardControl<ActionID>) {
         collision.bodies.resize_with(4, Aabb::default);
         collision.velocities.resize_with(4, Default::default);
         collision.metadata.resize_with(4, Default::default);
@@ -362,52 +426,5 @@ impl World {
                 }
             }
         }
-    }*/
-
-}
-
-
-impl Logics {
-    fn new() -> Self {
-        Self {
-            control: {
-                let mut control = BevyKeyboardControl::new();
-                control.add_key_map(0,
-                    KeyCode::Q,
-                    ActionID::MoveUp(Player::P1),
-                );
-                control.add_key_map(0,
-                    KeyCode::A,
-                    ActionID::MoveDown(Player::P1),
-                );
-                control.add_key_map(0,
-                    KeyCode::W,
-                    ActionID::Serve(Player::P1),
-                );
-                control.add_key_map(1,
-                    KeyCode::O,
-                    ActionID::MoveUp(Player::P2),
-                );
-                control.add_key_map(1,
-                    KeyCode::L,
-                    ActionID::MoveDown(Player::P2),
-                );
-                control.add_key_map(1,
-                    KeyCode::I,
-                    ActionID::Serve(Player::P2),
-                );
-                control
-            },
-            physics: PointPhysics::new(),
-            collision: AabbCollision::new(),
-            resources: {
-                let mut resources = QueuedResources::new();
-                resources.items.insert( PoolID::Points(Player::P1), 0.0 );
-                resources.items.insert( PoolID::Points(Player::P2), 0.0 );
-                resources
-            },
-        }
     }
-}
-
-
+*/
