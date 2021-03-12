81517b4677bbfd2b37cdcd4ec037570de7f55b76
cynthia <cxla2018@mymail.pomona.edu>
2021-03-11 17:22:02 -0800

    condense control logics into one keyboard logic + input wrappers
    MacroQuadKeyboardControl, BevyKeyboardControl,
    WinitKeyboardControl -> KeyboardControl, whose update function takes an
    InputWrapper alongside an InputHelper/event. i haven't tested/documented
    this yet and it might cause problems in the future (though i guess the
    original approach might also have had the same problems), so i might go
    back on it altogether....

--- asterism/src/control.rs
+++ asterism/src/control.rs
@@ -7,7 +7,8 @@
 //! We're currently trying to consider analog as well as digital inputs, but we haven't implemented
 //! controller support, so some of these fields don't really make sense yet.
 
-use macroquad::prelude::{is_key_down, KeyCode as MqKeyCode};
+use std::collections::BTreeSet;
+
 use winit::event::VirtualKeyCode;
 use winit_input_helper::WinitInputHelper;
 
@@ -17,26 +18,89 @@ trait Input {
     fn max(&self) -> f32;
 }
 
-/// Trait for generic keyboard control.
-pub trait KeyboardControl<ID, KeyCode, InputHandler>: Default
+/// Generic keyboard control.
+pub struct KeyboardControl<ID, KeyCode>
 where
     ID: Copy + Eq + Ord,
+    KeyCode: Copy,
 {
-    fn new() -> Self {
+    pub mapping: Vec<Vec<Action<ID, KeyCode>>>,
+    pub values: Vec<Vec<Values>>,
+}
+
+impl<ID: Copy + Eq + Ord, KeyCode: Copy> Default for KeyboardControl<ID, KeyCode> {
+    fn default() -> Self {
+        Self {
+            mapping: Vec::new(),
+            values: Vec::new(),
+        }
+    }
+}
+
+impl<ID: Copy + Eq + Ord, KeyCode: Copy> KeyboardControl<ID, KeyCode> {
+    pub fn new() -> Self {
         Self::default()
     }
 
     /// Checks and updates what inputs are being pressed every frame.
-    fn update(&mut self, events: &InputHandler);
-
-    fn mapping(&self) -> &Vec<Vec<Action<ID, KeyCode>>>;
-    fn mapping_mut(&mut self) -> &mut Vec<Vec<Action<ID, KeyCode>>>;
-    fn values(&self) -> &Vec<Vec<Values>>;
-    fn values_mut(&mut self) -> &mut Vec<Vec<Values>>;
+    pub fn update<InputHelper>(
+        &mut self,
+        event_wrapper: &mut impl InputWrapper<KeyCode, InputHelper>,
+        events: &InputHelper,
+    ) {
+        event_wrapper.update(
+            // "please stop trying to do things with iterators cynthia"
+            // absolutely not. i have to use my hard-won cs54 knowledge for _something_
+            // anyway this is probably expensive or something. the way we log input schemes is so nightmarish :smiling_face_with_tear_emoji:
+            &self
+                .mapping
+                .iter()
+                .flat_map(|actions| {
+                    actions
+                        .iter()
+                        .map(|Action { key_input, .. }| key_input.keycode)
+                        .collect::<Vec<_>>()
+                })
+                .collect::<Vec<_>>(),
+            events,
+        );
+        for (map, map_values) in self.mapping.iter().zip(self.values.iter_mut()) {
+            for (action, mut values) in map.iter().zip(map_values.iter_mut()) {
+                let Action {
+                    key_input,
+                    input_type,
+                    is_valid,
+                    ..
+                } = action;
+                let Values { value, changed_by } = &mut values;
+                match input_type {
+                    InputType::Digital => {
+                        if *is_valid {
+                            if event_wrapper.is_held(&key_input.keycode, events) {
+                                if !event_wrapper.is_pressed(&key_input.keycode, events) {
+                                    *changed_by = 0.0;
+                                } else {
+                                    *changed_by = 1.0;
+                                }
+                            } else if event_wrapper.is_released(&key_input.keycode, events) {
+                                *changed_by = -1.0;
+                            } else {
+                                *changed_by = 0.0;
+                            }
+                        }
+                    }
+                    InputType::Analog => todo!(),
+                }
+                *value = (*value + *changed_by)
+                    .max(key_input.min())
+                    .min(key_input.max());
+            }
+        }
+    }
 
     /// Returns the [Values] for the first action in the mapping with the given ID.
-    fn get_action(&self, id: ID) -> Option<Values> {
-        for (i, ..) in self.mapping().iter().enumerate() {
+    pub fn get_action(&self, id: ID) -> Option<Values> {
+        for (i, ..) in self.mapping.iter().enumerate() {
             if let Some(values) = self.get_action_in_set(i, id) {
                 return Some(values);
             }
@@ -45,31 +109,26 @@ where
     }
 
     /// Returns the [Values] for the action with the given ID in the given set of mappings.
-    fn get_action_in_set(&self, action_set: usize, id: ID) -> Option<Values> {
-        if let Some(i) = self.mapping()[action_set]
-            .iter()
-            .position(|act| act.id == id)
-        {
-            return Some(self.values()[action_set][i]);
+    pub fn get_action_in_set(&self, action_set: usize, id: ID) -> Option<Values> {
+        if let Some(i) = self.mapping[action_set].iter().position(|act| act.id == id) {
+            return Some(self.values[action_set][i]);
         }
         None
     }
 
     /// Adds a single keymap to the logic.
-    fn add_key_map(&mut self, locus_idx: usize, keycode: KeyCode, id: ID) {
-        if locus_idx >= self.mapping().len() {
-            self.mapping_mut()
-                .resize_with(locus_idx + 1, Default::default);
-            self.values_mut()
-                .resize_with(locus_idx + 1, Default::default);
+    pub fn add_key_map(&mut self, locus_idx: usize, keycode: KeyCode, id: ID) {
+        if locus_idx >= self.mapping.len() {
+            self.mapping.resize_with(locus_idx + 1, Default::default);
+            self.values.resize_with(locus_idx + 1, Default::default);
         }
-        self.mapping_mut()[locus_idx].push(Action {
+        self.mapping[locus_idx].push(Action {
             id,
             key_input: KeyInput { keycode },
             is_valid: false,
             input_type: InputType::Digital,
         });
-        self.values_mut()[locus_idx].push(Values {
+        self.values[locus_idx].push(Values {
             value: 0.0,
             changed_by: 0.0,
         });
@@ -116,295 +175,108 @@ pub struct Action<ID, KeyCode> {
     pub input_type: InputType,
 }
 
-/// Implementation of keyboard control for `winit_input_helper`.
-pub struct WinitKeyboardControl<ID: Copy + Eq + Ord> {
-    /// Keymaps for each player.
-    ///
-    /// An index in the outer vec, i.e. `mapping[i]`, points to a keymap for a player. Indexing
-    /// that vec will get you actual actions.
-    pub mapping: Vec<Vec<Action<ID, VirtualKeyCode>>>,
-    /// The values for each input in `mapping`.
-    pub values: Vec<Vec<Values>>,
-    // Invariants: mapping.len() == values.len(), mapping[i].inputs.len() == values[i].len()
-    last_frame_inputs: Vec<VirtualKeyCode>,
-    this_frame_inputs: Vec<VirtualKeyCode>,
+pub trait InputWrapper<KeyCode, InputHelper> {
+    fn update(&mut self, keys: &[KeyCode], events: &InputHelper);
+    fn is_pressed(&self, key: &KeyCode, events: &InputHelper) -> bool;
+    fn is_released(&self, key: &KeyCode, events: &InputHelper) -> bool;
+    fn is_held(&self, key: &KeyCode, events: &InputHelper) -> bool;
 }
 
-/// Implementation of keyboard control for Macroquad's input handler. See [WinitKeyboardControl] for
-/// documentation of fields.
-pub struct MacroQuadKeyboardControl<ID: Copy + Eq + Ord> {
-    pub mapping: Vec<Vec<Action<ID, MqKeyCode>>>,
-    pub values: Vec<Vec<Values>>,
-    last_frame_inputs: Vec<MqKeyCode>,
+use macroquad::prelude::{is_key_down, is_key_pressed, KeyCode as MqKeyCode};
+pub struct MacroquadInputWrapper {
     this_frame_inputs: Vec<MqKeyCode>,
+    last_frame_inputs: Vec<MqKeyCode>,
 }
 
-impl<ID> KeyboardControl<ID, VirtualKeyCode, WinitInputHelper> for WinitKeyboardControl<ID>
-where
-    ID: Copy + Eq + Ord,
-{
-    fn mapping(&self) -> &Vec<Vec<Action<ID, VirtualKeyCode>>> {
-        &self.mapping
+impl MacroquadInputWrapper {
+    pub fn new() -> Self {
+        Self {
+            this_frame_inputs: Vec::new(),
+            last_frame_inputs: Vec::new(),
+        }
     }
-    fn mapping_mut(&mut self) -> &mut Vec<Vec<Action<ID, VirtualKeyCode>>> {
-        &mut self.mapping
-    }
+}
 
-    fn values(&self) -> &Vec<Vec<Values>> {
-        &self.values
-    }
-    fn values_mut(&mut self) -> &mut Vec<Vec<Values>> {
-        &mut self.values
-    }
-
-    fn update(&mut self, events: &WinitInputHelper) {
-        for (map, map_values) in self.mapping.iter().zip(self.values.iter_mut()) {
-            for (action, mut values) in map.iter().zip(map_values.iter_mut()) {
-                let Action {
-                    key_input,
-                    input_type,
-                    is_valid,
-                    ..
-                } = action;
-                let Values { value, changed_by } = &mut values;
-                match input_type {
-                    InputType::Digital => {
-                        if events.key_held(key_input.keycode) {
-                            self.this_frame_inputs.push(key_input.keycode);
-                            if *is_valid {
-                                if let Some(..) = self
-                                    .last_frame_inputs
-                                    .iter()
-                                    .position(|vkc| *vkc == key_input.keycode)
-                                {
-                                    *changed_by = 0.0;
-                                } else {
-                                    *changed_by = 1.0;
-                                }
-                            }
-                        } else if *is_valid {
-                            if let Some(..) = self
-                                .last_frame_inputs
-                                .iter()
-                                .position(|vkc| *vkc == key_input.keycode)
-                            {
-                                *changed_by = -1.0;
-                            } else {
-                                *changed_by = 0.0;
-                            }
-                        }
-                    }
-                    InputType::Analog => todo!(),
-                }
-                *value = (*value + *changed_by)
-                    .max(key_input.min())
-                    .min(key_input.max());
+impl InputWrapper<MqKeyCode, ()> for MacroquadInputWrapper {
+    fn update(&mut self, keys: &[MqKeyCode], _events: &()) {
+        self.last_frame_inputs = std::mem::take(&mut self.this_frame_inputs);
+        for key in keys.iter() {
+            if is_key_pressed(*key) {
+                self.this_frame_inputs.push(*key);
             }
         }
-        self.last_frame_inputs.clear();
-        for input in self.this_frame_inputs.iter() {
-            self.last_frame_inputs.push(*input);
-        }
-        self.this_frame_inputs.clear();
+    }
+
+    fn is_pressed(&self, key: &MqKeyCode, _events: &()) -> bool {
+        is_key_pressed(*key)
+    }
+
+    fn is_released(&self, key: &MqKeyCode, _events: &()) -> bool {
+        let was_pressed = self
+            .last_frame_inputs
+            .iter()
+            .position(|key_pressed| key == key_pressed)
+            .is_some();
+        let is_not_pressed = self
+            .this_frame_inputs
+            .iter()
+            .position(|key_pressed| key == key_pressed)
+            .is_none();
+        was_pressed && is_not_pressed
+    }
+
+    fn is_held(&self, key: &MqKeyCode, _events: &()) -> bool {
+        is_key_down(*key)
     }
 }
 
-impl<ID> Default for WinitKeyboardControl<ID>
-where
-    ID: Copy + Eq + Ord,
-{
-    fn default() -> Self {
-        Self {
-            mapping: Vec::new(),
-            values: Vec::new(),
-            last_frame_inputs: Vec::new(),
-            this_frame_inputs: Vec::new(),
-        }
-    }
+pub struct WinitInputWrapper {
+    this_frame_keys: BTreeSet<VirtualKeyCode>,
+    last_frame_keys: BTreeSet<VirtualKeyCode>,
 }
 
-/// Macroquad doesn't have a keyboard handler type, so use the unit type.
-impl<ID> KeyboardControl<ID, MqKeyCode, ()> for MacroQuadKeyboardControl<ID>
-where
-    ID: Copy + Eq + Ord,
-{
-    fn mapping(&self) -> &Vec<Vec<Action<ID, MqKeyCode>>> {
-        &self.mapping
-    }
-    fn mapping_mut(&mut self) -> &mut Vec<Vec<Action<ID, MqKeyCode>>> {
-        &mut self.mapping
-    }
-
-    fn values(&self) -> &Vec<Vec<Values>> {
-        &self.values
-    }
-    fn values_mut(&mut self) -> &mut Vec<Vec<Values>> {
-        &mut self.values
-    }
-
-    /// Macroquad doesn't have an input handler type, so pass in a reference to the unit type
-    /// instead
-    fn update(&mut self, _events: &()) {
-        for (map, map_values) in self.mapping.iter().zip(self.values.iter_mut()) {
-            for (action, mut values) in map.iter().zip(map_values.iter_mut()) {
-                let Action {
-                    key_input,
-                    input_type,
-                    is_valid,
-                    ..
-                } = action;
-                let Values { value, changed_by } = &mut values;
-                match input_type {
-                    InputType::Digital => {
-                        if is_key_down(key_input.keycode) {
-                            self.this_frame_inputs.push(key_input.keycode);
-                            if *is_valid {
-                                if let Some(..) = self
-                                    .last_frame_inputs
-                                    .iter()
-                                    .position(|vkc| *vkc == key_input.keycode)
-                                {
-                                    *changed_by = 0.0;
-                                } else {
-                                    *changed_by = 1.0;
-                                }
-                            }
-                        } else if *is_valid {
-                            if let Some(..) = self
-                                .last_frame_inputs
-                                .iter()
-                                .position(|vkc| *vkc == key_input.keycode)
-                            {
-                                *changed_by = -1.0;
-                            } else {
-                                *changed_by = 0.0;
-                            }
-                        }
-                    }
-                    InputType::Analog => todo!(),
-                }
-                *value = (*value + *changed_by)
-                    .max(key_input.min())
-                    .min(key_input.max());
+impl InputWrapper<VirtualKeyCode, WinitInputHelper> for WinitInputWrapper {
+    fn update(&mut self, keys: &[VirtualKeyCode], events: &WinitInputHelper) {
+        self.last_frame_keys = std::mem::take(&mut self.this_frame_keys);
+        for key in keys.iter() {
+            if events.key_held(*key) {
+                self.this_frame_keys.insert(*key);
             }
         }
-        self.last_frame_inputs.clear();
-        for input in self.this_frame_inputs.iter() {
-            self.last_frame_inputs.push(*input);
-        }
-        self.this_frame_inputs.clear();
     }
-}
 
-impl<ID> Default for MacroQuadKeyboardControl<ID>
-where
-    ID: Copy + Eq + Ord,
-{
-    fn default() -> Self {
-        Self {
-            mapping: Vec::new(),
-            values: Vec::new(),
-            last_frame_inputs: Vec::new(),
-            this_frame_inputs: Vec::new(),
-        }
+    fn is_held(&self, key: &VirtualKeyCode, events: &WinitInputHelper) -> bool {
+        events.key_held(*key)
+    }
+
+    fn is_pressed(&self, key: &VirtualKeyCode, _events: &WinitInputHelper) -> bool {
+        self.this_frame_keys.contains(key) && !self.last_frame_keys.contains(key)
+    }
+
+    fn is_released(&self, key: &VirtualKeyCode, events: &WinitInputHelper) -> bool {
+        events.key_released(*key)
     }
 }
 
 #[cfg(feature = "bevy-engine")]
 use bevy_input::{keyboard::KeyCode as BevyKeyCode, Input as BevyInput};
 
-/// Implementation of keyboard control for Bevy's input handler. See [WinitKeyboardControl] for
-/// documentation of fields.
 #[cfg(feature = "bevy-engine")]
-pub struct BevyKeyboardControl<ID: Copy + Eq + Ord> {
-    pub mapping: Vec<Vec<Action<ID, BevyKeyCode>>>,
-    pub values: Vec<Vec<Values>>,
-    last_frame_inputs: Vec<BevyKeyCode>,
-    this_frame_inputs: Vec<BevyKeyCode>,
-}
+pub struct BevyInputWrapper;
 
 #[cfg(feature = "bevy-engine")]
-impl<ID> KeyboardControl<ID, BevyKeyCode, BevyInput<BevyKeyCode>> for BevyKeyboardControl<ID>
-where
-    ID: Copy + Eq + Ord,
-{
-    fn mapping(&self) -> &Vec<Vec<Action<ID, BevyKeyCode>>> {
-        &self.mapping
-    }
-    fn mapping_mut(&mut self) -> &mut Vec<Vec<Action<ID, BevyKeyCode>>> {
-        &mut self.mapping
+impl InputWrapper<BevyKeyCode, BevyInput<BevyKeyCode>> for BevyInputWrapper {
+    fn update(&mut self, _keys: &[BevyKeyCode], _events: &BevyInput<BevyKeyCode>) {}
+
+    fn is_held(&self, key: &BevyKeyCode, events: &BevyInput<BevyKeyCode>) -> bool {
+        events.pressed(*key)
     }
 
-    fn values(&self) -> &Vec<Vec<Values>> {
-        &self.values
-    }
-    fn values_mut(&mut self) -> &mut Vec<Vec<Values>> {
-        &mut self.values
+    fn is_pressed(&self, key: &BevyKeyCode, events: &BevyInput<BevyKeyCode>) -> bool {
+        events.just_pressed(*key)
     }
 
-    fn update(&mut self, events: &BevyInput<BevyKeyCode>) {
-        for (map, map_values) in self.mapping.iter().zip(self.values.iter_mut()) {
-            for (action, mut values) in map.iter().zip(map_values.iter_mut()) {
-                let Action {
-                    key_input,
-                    input_type,
-                    is_valid,
-                    ..
-                } = action;
-                let Values { value, changed_by } = &mut values;
-                match input_type {
-                    InputType::Digital => {
-                        if events.pressed(key_input.keycode) {
-                            self.this_frame_inputs.push(key_input.keycode);
-                            if *is_valid {
-                                if let Some(..) = self
-                                    .last_frame_inputs
-                                    .iter()
-                                    .position(|vkc| *vkc == key_input.keycode)
-                                {
-                                    *changed_by = 0.0;
-                                } else {
-                                    *changed_by = 1.0;
-                                }
-                            }
-                        } else if *is_valid {
-                            if let Some(..) = self
-                                .last_frame_inputs
-                                .iter()
-                                .position(|vkc| *vkc == key_input.keycode)
-                            {
-                                *changed_by = -1.0;
-                            } else {
-                                *changed_by = 0.0;
-                            }
-                        }
-                    }
-                    InputType::Analog => todo!(),
-                }
-                *value = (*value + *changed_by)
-                    .max(key_input.min())
-                    .min(key_input.max());
-            }
-        }
-        self.last_frame_inputs.clear();
-        for input in self.this_frame_inputs.iter() {
-            self.last_frame_inputs.push(*input);
-        }
-        self.this_frame_inputs.clear();
-    }
-}
-
-#[cfg(feature = "bevy-engine")]
-impl<ID> Default for BevyKeyboardControl<ID>
-where
-    ID: Copy + Eq + Ord,
-{
-    fn default() -> Self {
-        Self {
-            mapping: Vec::new(),
-            values: Vec::new(),
-            last_frame_inputs: Vec::new(),
-            this_frame_inputs: Vec::new(),
-        }
+    fn is_released(&self, key: &BevyKeyCode, events: &BevyInput<BevyKeyCode>) -> bool {
+        events.just_released(*key)
     }
 }
